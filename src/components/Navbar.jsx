79c5eea96694829ea5b3824e448b16b6a086343b
Deepesh Salhotra <yoii186@Deepeshs-MacBook-Air.local>
2024-12-16 23:28:49 +0530

    new init

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export const Navbar = () => {
+    return(
+        <div className="border-b border-gray-400 shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1)] pb-4 mt-2">
+            <div className="flex justify-between items-center">
+                <img src="./src/assets/AZP.png" alt="AZP" className="h-[20%] w-[10%] pl-2"/>
+                <img src="./src/assets/az.jpeg" alt="AZ-Logo" className="h-[20%] w-[10%] pr-3"/>
+            </div>
+        </div>
+    )
+}
