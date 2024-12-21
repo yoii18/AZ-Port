68f4791a1e994219165014f87077c358ce4f92a4
Deepesh Salhotra <yoii186@Deepeshs-MacBook-Air.local>
2024-12-21 10:10:26 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import {Navbar} from './components/Navbar'
+import { Main } from './components/Main'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Main />
     </>
   )
 }
